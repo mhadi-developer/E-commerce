47ba3db466fcd3d8115299fc65b172e7dc7d10f3
Muhammad Hadi <hadiquershi08@gmail.com>
2026-02-26 14:55:34 +0500

    Deployemnt Started

--- Frontend/src/components/nav.jsx
+++ Frontend/src/components/nav.jsx
@@ -107,7 +107,7 @@ export default function Nav({ categories }) {
                     </a>
                   </div>
                   <div className="navbar-nav ml-auto py-0 d-none d-lg-block">
-                    <a href="" className="btn px-0">
+                    <a href="" className="px-0">
                       <i className="fas fa-heart text-primary"></i>
                       <span
                         className="badge text-secondary border border-secondary rounded-circle"
@@ -116,7 +116,7 @@ export default function Nav({ categories }) {
                         0
                       </span>
                     </a>
-                    <Link to={'/cart'} className="btn px-0 ml-3">
+                    <Link to={'/cart'} className=" px-0 ml-3">
             
                      <i className="fas fa-shopping-cart text-primary"></i>
                       <span
